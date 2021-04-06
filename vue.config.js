895b1f3a43585ca27fecc7303f7784c8d92cf476
Administrator <>
2021-04-06 22:25:50 +0800

    本地导入

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        assets: '@/assets',
+        common: '@/common',
+        components: '@/components',
+        network: '@/network',
+        views: '@/views'
+      }
+    }
+  },
   devServer: {
-    port: 8081 // 端口号
+    port: 8082 // 端口号
   }
 }
